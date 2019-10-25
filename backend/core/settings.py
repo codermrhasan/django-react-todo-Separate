13f15cddb029792fdb811383cabd083c53f433b6
Rakib Hasan <hasanthecoder@gmail.com>
2019-10-25 14:43:23 +0600

    creating todos app model and preparing frontend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'rest_framework',
+    'corsheaders',
+
+    # Local apps
+    'todos.apps.TodosConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -47,6 +55,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'core.urls'
